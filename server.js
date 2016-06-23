b313677debe30316fd43d7b0bb1c6fda70c3f87b
bajwap <bajwap@umich.edu>
2016-06-22 22:35:30 -0400

    cleaned up node modules

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
-var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
